a9f69445feb0297350e584f01a55dc2c0dd3c104
Oğuzhan Aydoğan <negatif25@gmail.com>
2025-07-17 14:13:50 +0300

    no message

--- Services/ChatService.cs
+++ Services/ChatService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Repositories.DTOs;
+﻿using Repositories.DTOs;
 using Repositories.Entities;
 using Repositories.Repositories;
 
--- backend/Controllers/ChatController.cs
+++ backend/Controllers/ChatController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Repositories.Entities;
 using Services;
 using System.Text.Json;
 
